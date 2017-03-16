6610943ada7e101cbca1add3a8e601a50d294f26
evilkid <yassine_hell@hotmail.com>
2017-03-16 11:17:36 +0100

    Added new methods

--- java-source/src/main/java/com/example/api/ExampleApi.java
+++ java-source/src/main/java/com/example/api/ExampleApi.java
@@ -2,23 +2,23 @@ package com.example.api;
 
 import net.corda.core.contracts.*;
 import net.corda.core.crypto.Party;
+import net.corda.core.crypto.SecureHash;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.messaging.FlowHandle;
 import net.corda.core.node.NodeInfo;
 import net.corda.core.node.ServiceEntry;
 import net.corda.core.serialization.OpaqueBytes;
+import net.corda.core.transactions.SignedTransaction;
 import net.corda.flows.CashFlowCommand;
 import net.corda.flows.IssuerFlow;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Collections.singletonMap;
 import static java.util.stream.Collectors.toList;
@@ -40,6 +40,7 @@ public class ExampleApi {
     public ExampleApi(CordaRPCOps services) {
         this.myLegalName = services.nodeIdentity().getLegalIdentity().getName();
         this.services = services;
+
         updatePeers();
         updateIssuers();
         updateNotaries();
@@ -59,15 +60,16 @@ public class ExampleApi {
 
             Party party = services.partyFromName(peerName);
 
-            CashFlowCommand.IssueCash cash = new CashFlowCommand.IssueCash(new Amount<>(quantity, ContractsDSL.USD), OpaqueBytes.Companion.of((byte) 1), party, notaries.get(0));
+
+            CashFlowCommand.IssueCash cash = new CashFlowCommand.IssueCash(new Amount<>((long) quantity, ContractsDSL.USD), OpaqueBytes.Companion.of((byte) 1), party, notaries.get(0));
             System.out.println("created");
 
             FlowHandle handle = services.startFlowDynamic(IssuerFlow.IssuanceRequester.class, cash.getAmount(), cash.getRecipient(), cash.getIssueRef(), services.nodeIdentity().getLegalIdentity());
             System.out.println("handeled");
-            handle.getReturnValue().get();
+            SignedTransaction signedTransaction = (SignedTransaction) handle.getReturnValue().get(10 * 1000, TimeUnit.MILLISECONDS);
             System.out.println("exec");
 
-            return "issued";
+            return signedTransaction.getId().toString();
         } catch (Exception e) {
             e.printStackTrace();
             return e.getMessage();
@@ -85,8 +87,6 @@ public class ExampleApi {
             return "Peer not found";
         }
 
-
-
         try {
             System.out.println("Issuer: " + issuers.get(0));
 
@@ -94,22 +94,72 @@ public class ExampleApi {
             Amount<Issued<Currency>> amount = new Amount<>(quantity, new Issued<>(new PartyAndReference(issuers.get(0), OpaqueBytes.Companion.of((byte) 1)), ContractsDSL.USD));
             CashFlowCommand.PayCash cash = new CashFlowCommand.PayCash(amount, party);
 
-            FlowHandle handle = cash.startFlow(services);
+            FlowHandle<SignedTransaction> handle = cash.startFlow(services);
 
             //SignedTransaction a = new SignedTransaction();
             System.out.println(handle.getReturnValue().get().getClass());
 
-            return handle.getReturnValue().get().toString();
+            return handle.getReturnValue().get(10 * 1000, TimeUnit.MILLISECONDS).toString();
         } catch (Exception e) {
             return e.getMessage();
         }
     }
 
     @GET
+    @Path("exit/{amount}")
+    public String exit(@PathParam("amount") int quantity) {
+        try {
+
+
+            Amount<Currency> amount = new Amount<>((long) quantity, ContractsDSL.USD);
+
+            System.out.println(amount);
+
+            if (issuers.isEmpty()) {
+                updateIssuers();
+            }
+
+            CashFlowCommand.ExitCash exitCash = new CashFlowCommand.ExitCash(amount, issuers.get(0).ref(OpaqueBytes.Companion.of((byte) 1)).getReference());
+
+            FlowHandle<SignedTransaction> handle = exitCash.startFlow(services);
+
+
+            return handle.getReturnValue().get(10 * 1000, TimeUnit.MILLISECONDS).toString();
+        } catch (Exception e) {
+
+            return e.getMessage();
+        }
+
+    }
+
+    @GET
     @Path("vault")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<StateAndRef<ContractState>> getPurchaseOrders() {
+    public List<StateAndRef<ContractState>> getAllTransactions() {
         return services.vaultAndUpdates().getFirst();
+    }
+
+
+    @GET
+    @Path("vault/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public StateAndRef<ContractState> getTransactionById(@PathParam("id") String id) {
+
+        for (StateAndRef stateAndRef : services.vaultAndUpdates().getFirst()) {
+
+            if (stateAndRef.getRef().getTxhash().equals(SecureHash.parse(id))) {
+                return stateAndRef;
+            }
+        }
+
+        throw new NotFoundException("Could not find transaction");
+    }
+
+    @GET
+    @Path("balance")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<Currency, Amount<Currency>> getBalance() {
+        return services.getCashBalances();
     }
 
     @GET
@@ -120,30 +170,60 @@ public class ExampleApi {
         return issuers.stream().map(Party::getName).collect(toList());
     }
 
+
+    @GET
+    @Path("issuers/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Party getIssuerByName(@PathParam("name") String name) {
+        updateIssuers();
+        return issuers.stream()
+                .filter(party -> party.getName().equals(name))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
     @GET
     @Path("peers")
     @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, List<String>> getPeers() {
-        return singletonMap(
-                "peers",
-                services.networkMapUpdates().getFirst()
-                        .stream()
-                        .map(node -> node.getLegalIdentity().getName())
-                        .filter(name -> !name.equals(myLegalName) && !name.equals(NOTARY_NAME))
-                        .collect(toList()));
+    public List<String> getPeers() {
+        updatePeers();
+        return services.networkMapUpdates().getFirst()
+                .stream()
+                .map(node -> node.getLegalIdentity().getName())
+                .filter(name -> !name.equals(myLegalName) && !name.equals(NOTARY_NAME))
+                .collect(toList());
+    }
+
+    @GET
+    @Path("peers/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Party getPeerByName(@PathParam("name") String name) {
+        updateIssuers();
+        return peers.stream()
+                .filter(party -> party.getName().equals(name))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 
     @GET
     @Path("notaries")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Party> getNotaryList() {
-        /*new CashCommand.IssueCash(new Amount<Currency>(10, ContractsDSL.USD), OpaqueBytes.Companion.of(1), services.nodeIdentity().getLegalIdentity() );
-        services.currentNodeTime();*/
-        //return "test " + new NetworkIdentityModel().getNotaries().size();
         if (notaries.isEmpty()) {
             updateNotaries();
         }
         return notaries;
+    }
+
+    @GET
+    @Path("peers/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Party getNotariesByName(@PathParam("name") String name) {
+        updateIssuers();
+        return notaries.stream()
+                .filter(party -> party.getName().equals(name))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 
     @GET
